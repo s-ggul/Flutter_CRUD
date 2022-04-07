652302ceed0dd9abd3e9db1365d3e300d8b0b51a
s-ggul <k0s0a7@gmail.com>
2022-04-07 22:48:09 +0900

    [add] routing

---
+++ lib/screens/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './edit.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+              context, CupertinoPageRoute(builder: (context) => EditPage()));
+        },
+        // 페이지 라우팅을 담당하는 부분의 코드 
+        // 여기서는 CupertinoPageRouter를 사용했음
+        tooltip: '노트를 추가하려면 클릭하세요',
+        label: Text('메모 추가'),
+        icon: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
