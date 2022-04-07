f67c8242db2fb53304c24740b14f5cb278d206d9
s-ggul <k0s0a7@gmail.com>
2022-04-08 01:52:35 +0900

    [fix] Android SDK Version

---
+++ flutter_crud/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'c:\/Users/k0s0a/Desktop/lib/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
