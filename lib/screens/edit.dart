721ab836361ca74058df81a75e99314ea44ca894
s-ggul <k0s0a7@gmail.com>
2022-04-10 01:02:54 +0900

    [test] init test

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 class EditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue
-    );
+    return Container(color: Colors.blue);
   }
 }
+// test
